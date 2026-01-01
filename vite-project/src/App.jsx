5fa64c92fa2ef94400c160dc2206a9bce796b78f
Karthik171819 <karthikkumar092004@gmail.com>
2026-01-01 10:38:17 +0530

    Portfolio

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+       <Routes>
+        <Route index element={}/>
+       </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
