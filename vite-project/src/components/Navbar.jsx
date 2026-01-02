7d42c26faa8de49ac4eb9c013c8772af2a25fc11
Karthik171819 <karthikkumar092004@gmail.com>
2026-01-02 18:20:22 +0530

    Portfolio

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,39 +1,58 @@
 import { cn } from "@/lib/utils";
 import { useEffect, useState } from "react";
 
-const navItems =[
-    { name: "Home", href: "#hero"},
-    { name: "About", href: "#about"},
-    { name: "Skills", href: "#skills"},
-    { name: "Projects", href: "#projects"},
-    { name: "Contact", href: "#contact"},
+const navItems = [
+  { name: "Home", href: "#hero" },
+  { name: "About", href: "#about" },
+  { name: "Skills", href: "#skills" },
+  { name: "Projects", href: "#projects" },
+  { name: "Contact", href: "#contact" },
 ];
 
 export const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
 
-    const [isScrolled, setIsScrolled] = useState(false);
+  //useEffect to handle scroll event
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
 
-    //useEffect to handle scroll event
-    useEffect(() =>{
-        const handleScroll = () => {
-            setIsScrolled(window.screenY > 10)
-        }
-
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-    return <nav className={cn(
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <nav
+      className={cn(
         "fixed w-full z-40 transition-all duration-300",
-        isScrolled ? "bg-background/80 backdrop-blur-md shadow-md py-3" : "py-5"
-    )}>
+        isScrolled ? "bg-background/80 backdrop-blur-md shadow-sm py-3" : "py-5"
+      )}
+    >
+      <div className="container flex items-center justify-between">
+        {/* Logo of portfolio */}
+        <a
+          className="text-xl font-bold text-primary flex items-center"
+          href="#hero"
+        >
+          <span className="relative z-10">
+            <span className="text-glow text-foreground">Karthik</span>{" "}
+            <span className="text-accent">Portfolio</span>
+          </span>
+        </a>
 
-        <div className="container flex items-center justify-between">
-            {/* Logo of portfolio */}
-            <a className="text-xl font-bold text-primary flex items-center">
-                <span className="relative z-10">
-                    <span className="text-glow text-foreground">Karthik</span> Portfolio
-                </span>
+        {/* desktop nav} */}
+        <div className="hidden md:flex space-x-8">
+          {navItems.map((item, key) => (
+            <a
+              key={key}
+              href={item.href}
+              className="text-foreground hover:text-primary transition"
+            >
+              {item.name}
             </a>
+          ))}
         </div>
+      </div>
     </nav>
+  );
 };
