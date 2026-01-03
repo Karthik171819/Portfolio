404c5106b311cdc31c9903c8cd90b928c61a0f4e
Karthik171819 <karthikkumar092004@gmail.com>
2026-01-03 15:28:27 +0530

    Portfolio

--- vite-project/src/components/ProjectSection.jsx
+++ vite-project/src/components/ProjectSection.jsx
@@ -1,3 +1,5 @@
+import { ExternalLink, Github } from "lucide-react";
+
 const projects = [
   {
     id: 1,
@@ -26,7 +28,7 @@ const projects = [
     description:
       "Interactive movie-store web application with smooth user intutive using React",
     image: "/projects/project3.png",
-    tags: ["JavaScript", "React", "tailwindCSS", "API"],
+    tags: ["JavaScript", "React",  "API"],
     demourl: "#",
     githubUrl: "#",
   },
@@ -42,9 +44,49 @@ export const ProjectSection = () => {
         </h2>
 
         <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
-            here are some of my recent projects. Each project was carefully
-            crafted with attention to detail, performance, and user experience.
+          here are some of my recent projects. Each project was carefully
+          crafted with attention to detail, performance, and user experience.
         </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projects.map((project, key) => (
+            <div
+              key={key}
+              className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
+            >
+              {/* image banner */}
+              <div className="h-48 overflow-hidden">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                />
+              </div>
+                <div className="p-6">
+                    <div className="flex flex-wrap gap-2 mb-4">
+                        {project.tags.map((tag) => (
+                            <span className="px-2 py-1 text-sm font-medium border rounded-full bg-secondary text-secondary-foreground">
+                                {tag}
+                                </span>
+                        ))}
+                    </div>  
+                </div>
+                <h3 className="text-xl font-semibold mb-2 text-center">{project.title}</h3>
+                <p className="text-muted-foreground text-sm mb-4">{project.description}</p>
+                <div className="flex justify-between items-center">
+                    <div className="flex space-x-3">
+                        <a href="{project.demoUrl}">
+                            {" "}
+                            <ExternalLink/>
+                        </a>
+                        <a href="{project.demoUrl}">
+                            <Github/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
