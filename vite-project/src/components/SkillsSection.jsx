8c22ff8990d07e4dc7ff61c8129de4ab2d761d65
Karthik171819 <karthikkumar092004@gmail.com>
2026-01-03 13:12:14 +0530

    Portfolio

--- vite-project/src/components/SkillsSection.jsx
+++ vite-project/src/components/SkillsSection.jsx
@@ -27,6 +27,16 @@ export const SkillsSection = () => {
             <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
                 My <span className="text-accent">Skills</span>
             </h2>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                {skills.map((skill, key) => (
+                    <div key={key} className="bg-card p-6 rounded-lg shadow-xs card-hover">
+                        <div className="text-left mb-4">
+                            <h3 className="font-semibold text-lg"> {skill.name}</h3>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     </section>
   );
