580444e3574bd515eab0f776a746a629884d5cf2
Karthik171819 <karthikkumar092004@gmail.com>
2026-01-03 20:39:43 +0530

    Portfolio

--- vite-project/src/components/ContactSection.jsx
+++ vite-project/src/components/ContactSection.jsx
@@ -1,3 +1,37 @@
+import { Mail } from "lucide-react"
+
 export const ContactSection = () => {
-    return <section></section>
+    return <section  id="contact" className="py-24 px-4 relative bg-secondary/30">
+        <div className="container mx-auto max-w-5xl">
+            <h2 className="text=3xl md:text-4xl font-bold mb-4 text-center">
+                Get In <span className="text-accent"> Touch </span>
+            </h2>
+
+            <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+                  I’m actively looking for entry-level opportunities as a JavaScript Full-Stack Developer.
+                  I enjoy building real-world web applications and continuously improving my skills.
+                  If you’d like to collaborate or think I’d be a good fit for your team, feel free to reach out.
+            </p>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+                <div className="space-y-8">
+                   <h3 className="text-2xl font-semibold mb-6"> Contact Information</h3> 
+
+                   <div className="space-y-6 justify-center">
+                       <div className="flex items-start space-x-4">
+                           <div className="p-3 rounded-full bg-accent/10">
+                            <Mail className="h-6 w-6 text-accent"/> {" "}
+                           </div> 
+                           <div>
+                                <h4 className=""> Email </h4>
+                                <a href="mailto:karthikkumar092004@gmail.com">
+
+                                </a>
+                           </div>
+                       </div>
+                   </div>
+                </div>
+            </div>
+        </div>  
+    </section>
 }
