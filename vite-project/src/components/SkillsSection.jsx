699b0fec96983065bf764160538a0956f0c97ef9
Karthik171819 <karthikkumar092004@gmail.com>
2026-01-03 12:37:39 +0530

    Portfolio

--- vite-project/src/components/SkillsSection.jsx
+++ vite-project/src/components/SkillsSection.jsx
@@ -1,8 +1,22 @@
 const skills = [
     //frontend
     {name: "HTML/CSS", level: 95, category: "frontend" },
-    {name:"JavaScript", level: 90, category: "frontend"},
-]
+    {name: "JavaScript", level: 90, category: "frontend"},
+    {name: "React", level: 90, category: "frontend"},
+    {name: "Tailwind CSS", level:85, category: "frontend"},
+
+    //Backend
+    { name: "Node.js", level: 80, category: "backend"},
+    { name: "Express",  level: 80, category: "backend"},
+    { name: "Python", level: 80, category: "backend"},
+    { name: "MongoDB", level: 80, category: "backend"},
+    { name: "MySQL", level: 80, category: "backend"},
+
+    //Tools
+    { name: "Git/GitHub", level: 90, category: "tools"},
+    { name: "VS Code", level: 80, category: "tools"},
+
+];
 
 
 export const SkillsSection = () => {
