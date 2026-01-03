e966a0db29b3564f40d6b064d7130c4e18b67b73
Karthik171819 <karthikkumar092004@gmail.com>
2026-01-03 12:27:15 +0530

    Portfolio

---
+++ vite-project/src/components/SkillsSection.jsx
@@ -0,0 +1,10 @@
+const skills = [
+    //frontend
+    {name: "HTML/CSS", level: 95, category: "frontend" },
+    {name:"JavaScript", level: 90, category: "frontend"},
+]
+
+
+export const SkillsSection = () => {
+    return <section></section>
+}
